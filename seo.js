69e1f948f2086ea3f66b9c23239642d96e977e5f
Nick Wientge <nick@exygeninteractive.com>
2014-09-17 13:53:18 -0700

    fix favicons

--- seo.js
+++ seo.js
@@ -7,6 +7,10 @@ Meteor.startup(function() {
       },
       og: {
         'image': Meteor.absoluteUrl('share-image.png')
+      },
+      ignore: {
+        meta: ['fragment', 'viewport', 'msapplication-TileColor', 'msapplication-TileImage', 'msapplication-config'],
+        link: ['stylesheet', 'apple-touch-icon', 'rel', 'shortcut icon', 'icon']
       }
     });
   }
