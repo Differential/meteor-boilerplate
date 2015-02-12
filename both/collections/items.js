b4be03b317597532c2783a5ad18949dee9118df6
gerard sychay <hellogerard@gmail.com>
2015-02-11 23:40:06 -0500

    Add some default authorization rules

--- both/collections/items.js
+++ both/collections/items.js
@@ -7,3 +7,23 @@ Items.helpers({
 Items.before.insert(function (userId, doc) {
   doc.createdAt = moment().toDate();
 });
+
+Items.allow({
+  insert: function(userId, doc) {
+    return !!userId;
+  },
+
+  update: function(userId, doc, fields) {
+    var _fields = _(fields);
+
+    if (_fields.contains('userId')) {
+      return false;
+    }
+
+    return !!userId && doc.userId === userId;
+  },
+
+  remove: function(userId, doc) {
+    return !!userId && doc.userId === userId;
+  }
+});
