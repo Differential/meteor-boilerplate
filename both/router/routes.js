7bc366a6822bec910a81eb243cc70689c564ed4f
Nick Wientge <nick@exygeninteractive.com>
2015-03-09 18:32:45 -0700

    fix conflicts in routes

--- both/router/routes.js
+++ both/router/routes.js
@@ -2,17 +2,14 @@ Router.route('/', {
   name: 'home'
 });
 
-<<<<<<< HEAD
-Router.route('/dashboard');
-
-Router.route('/items/new', {
-  name: 'items.new'
-=======
 Router.route('/dashboard', {
   name: 'dashboard'
 });
 
+Router.route('/items/new', {
+  name: 'items.new'
+});
+
 Router.plugin('ensureSignedIn', {
   only: ['dashboard']
->>>>>>> origin/master
 });
